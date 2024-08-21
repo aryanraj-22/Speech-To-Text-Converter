00125b67f33db01571175535bb445cc04c8a90c8
Aryan Raj <157687923+aryanraj-22@users.noreply.github.com>
2024-08-21 17:48:34 +0530

    Add files via upload

---
+++ SpeechToText/languages-option.js
@@ -0,0 +1,76 @@
+
+const languages = [
+    {
+      no: "1",
+      name: "English",
+      native: "English",
+      code: "en",
+    },
+    {
+      no: "2",
+      name: "Arabic",
+      native: "عربي",
+      code: "ar",
+    },
+    {
+      no: "3",
+      name: "French",
+      native: "Français",
+      code: "fr",
+    },
+    {
+      no: "4",
+      name: "German",
+      native: "Deutsch",
+      code: "de",
+    },
+    {
+      no: "5",
+      name: "Hindi",
+      native: "हिन्दी",
+      code: "hi",
+    },
+    {
+      no: "6",
+      name: "Italian",
+      native: "Italiano",
+      code: "it",
+    },
+    {
+      no: "7",
+      name: "Japanese",
+      native: "日本語",
+      code: "ja",
+    },
+    {
+      no: "8",
+      name: "Korean",
+      native: "한국어",
+      code: "ko",
+    },
+    {
+      no: "9",
+      name: "Spanish",
+      native: "Español",
+      code: "es",
+    },
+    {
+      no: "10",
+      name: "Urdu",
+      native: "اردو",
+      code: "ur",
+    },
+    {
+        no: "11",
+        name: "Tamil",
+        native: "தமிழ்",
+        code: "ta",
+    },
+    {
+        no: "12",
+        name: "Punjabi",
+        native: "ਪੰਜਾਬੀ",
+        code: "pa"
+
+    },
+  ];
